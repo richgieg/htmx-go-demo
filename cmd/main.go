dc03fb1e079cca83c135c85b8b2946b0d8bb4d24
Richard Gieg <richgieg@users.noreply.github.com>
2024-10-06 18:43:58 +0000

    Begin adding contact form and contacts display

--- cmd/main.go
+++ cmd/main.go
@@ -22,6 +22,33 @@ func newTemplate() *Templates {
 	}
 }
 
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("John", "jd@gmail.com"),
+			newContact("Clara", "cd@gmail.com"),
+		},
+	}
+}
+
 type Count struct {
 	Count int
 }
@@ -30,16 +57,19 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	count := Count{Count: 0}
+	data := newData()
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", data)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		data.Contacts = append(data.Contacts, newContact(name, email))
+		return c.Render(200, "display", data)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
